b67bb1634c82c8386c39ff4320df1396ebba7560
MuhammadMuso <bdjdkdnzn@gmail.com>
2025-12-02 13:57:42 +0500

    ajoyib

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
-from .views import index
+from django.urls import path
+from .views import index, create_order, clients
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('create_order/', create_order, name='create_order'),
+    path('clients/', clients, name='clients'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import CarpetType, Order, VisitorCount, Review
+from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
-    orders = Order.objects.all()
     carpets = CarpetType.objects.all()
     reviews = Review.objects.all().order_by('-created_at')
 
@@ -15,7 +15,6 @@ def index(request):
     total_visitors = VisitorCount.objects.count()
 
     context = {
-        'orders': orders,
         'carpets': carpets,
         'reviews': reviews,
         'total_visitors': total_visitors,
@@ -27,3 +26,31 @@ def index(request):
         response.set_cookie('visited', 'yes', max_age=365*24*60*60)
 
     return response
+def create_order(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
+        address = request.POST.get("address")
+        carpet_type_id = request.POST.get("carpet_type")
+        other_carpet_name = request.POST.get("other_carpet_name")
+        date = request.POST.get("date")
+        comment = request.POST.get("comment")
+
+        carpet_type = CarpetType.objects.get(id=carpet_type_id) if carpet_type_id else None
+
+        Order.objects.create(
+            name=name,
+            phone=phone,
+            address=address,
+            carpet_type=carpet_type,
+            other_carpet_name=other_carpet_name,
+            date=date,
+            comment=comment
+        )
+        return redirect('index')
+    return redirect('index')
+
+
+def clients(request):
+    orders = Order.objects.all().order_by('-id') 
+    return render(request, 'clients.html', {'orders': orders})
